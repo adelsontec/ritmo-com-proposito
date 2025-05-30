701941fdb578133a47e0483e9d614dab79e83cc2
adelsontec <adelsontec@gmail.com>
2025-05-30 09:26:02 -0300

    Adiciona PWA completo com manifest e ícones

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1e293b" />
+        <link rel="icon" href="/icons/icon-192.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
